56e9693a173361c1fc3aa0a6fdbe14b2d5e1eb51
Paul Stadig <paul@stadig.name>
2013-01-29 06:32:14 -0500

    Bump to 0.0.4.

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
-(defproject nio "0.0.4-SNAPSHOT"
+(defproject nio "0.0.4"
   :description "Clojure support for java.nio."
+  :url "http://github.com/pjstadig/nio/"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dev-dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}})
